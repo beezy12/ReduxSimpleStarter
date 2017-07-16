631de57e5911bf7bb133e81e5537cfbdbaed16e9
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2017-07-16 14:00:06 -0500

    added notes

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,9 +1,26 @@
+// by writing the Component in curly braces here, it's the same thing as saying:
+// const Component = React.Component;
+import React, { Component } from 'react';
 
-import React from 'react';
+// don't have to write "extends React.Component" because I am pulling out 'Component' above.
+// As opposed to functional components, class-based components allow me to handle state
+// and require a 'render'. start off with a functional component, and make it a class if you need it.
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
 
+        this.state = { term: '' };
+    }
 
-const SearchBar = () => {
-    return <input />
-};
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
+    }
+}
 
 export default SearchBar;
